911ae60bc57c55549a42a36008e338aba7167441
ned <ned@appliedtrust.com>
2011-12-03 08:50:03 -0700

    started work on parsing wiki page

--- ipad/ATkeg/ViewController.m
+++ ipad/ATkeg/ViewController.m
@@ -78,57 +78,80 @@
 
 - (void)doKegDataRefresh
 {
-	//
+	// configurables
+    NSString       *kegbotURL = @"http://localhost/check.php";
+    NSString       *wikiURL = @"http://localhost/wiki.html";
+    
+    //
     NSLog(@"Refresh started...");
     self.navBar.topItem.title = @"Loading...";
     
-    // fetch the data
+    
+    NSLog(@"Fetching wiki page..."); //wikiURL
     NSError        *urlerror = nil;
     NSURLResponse  *response = nil;
-    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"http://bull/kegbot/check.php"]];
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:wikiURL]];
     self.responseData = [NSURLConnection sendSynchronousRequest:request returningResponse: &response error: &urlerror];
-    //urlerror = nil; // XXX remove this after debugging
+    urlerror = nil; // XXX remove this after debugging
     if (urlerror) { 
         NSLog(@"Handle this fetch error.");
         self.navBar.topItem.title = @"Error Fetching Keg Status";
+        NSLog(@"Refresh failed!");
+        
+    } else {
+        NSString       *kegdata =    [[NSString alloc] initWithData:responseData encoding:NSUTF8StringEncoding];
+        
+        NSLog(@"kegdata from wiki: %@", kegdata);
+        
+        //NSLog(@"kegdata: %@", kegdata);
+        //temp1,temp2,%remaining_left_side,%remaining_right_side,freemem_in_bits    
+        //NSArray* kegdataparts = [kegdata componentsSeparatedByString: @","];
+        
+        /*
+         NSRegularExpression *regex = [NSRegularExpression 
+         regularExpressionWithPattern:@"^...."
+         options:0
+         error:&error];
+         
+         NSRange range   = [regex rangeOfFirstMatchInString:kegdata options:0 range:NSMakeRange(0, [kegdata length])];
+         NSString *output = [kegdata substringWithRange:range];
+         NSLog(@"parse result: %@", output);
+         */
+        
+        
+        NSLog(@"got the wiki data!");
+    }
+    
+    NSLog(@"Fetching arduino data...");
+
+    // fetch the data
+    urlerror = nil;
+    response = nil;
+    request = [NSURLRequest requestWithURL:[NSURL URLWithString:kegbotURL]];
+    self.responseData = [NSURLConnection sendSynchronousRequest:request returningResponse: &response error: &urlerror];
+    urlerror = nil; // XXX remove this after debugging
+    if (urlerror) { 
+        NSLog(@"Handle this fetch error.");
+        self.navBar.topItem.title = @"Error Fetching Keg Status";
+        NSLog(@"Refresh failed!");
 
     } else {
         NSString       *kegdata =    [[NSString alloc] initWithData:responseData encoding:NSUTF8StringEncoding];
     
-       // kegdata = @"32,31.5,99.96,2.65,811"; // XXX remove this after debugging
+        kegdata = @"32,31.5,99.96,2.65,811"; // XXX remove this after debugging
 
-        //temp1,temp2,%remaining_left_side,%remaining_right_side,freemem_in_bits
-        NSLog(@"kegdata: %@", kegdata);
+        //NSLog(@"kegdata: %@", kegdata);
+        //temp1,temp2,%remaining_left_side,%remaining_right_side,freemem_in_bits    
+        NSArray* kegdataparts = [kegdata componentsSeparatedByString: @","];
     
-    /*
-     NSRegularExpression *regex = [NSRegularExpression 
-     regularExpressionWithPattern:@"^...."
-     options:0
-     error:&error];
-     
-     NSRange range   = [regex rangeOfFirstMatchInString:kegdata options:0 range:NSMakeRange(0, [kegdata length])];
-     NSString *output = [kegdata substringWithRange:range];
-     NSLog(@"parse result: %@", output);
-     */
-    
-    NSArray* kegdataparts = [kegdata componentsSeparatedByString: @","];
-    
-    /*
-     NSLog(@"temp1: %@", [kegdataparts objectAtIndex: 0]);
-     NSLog(@"temp2: %@", [kegdataparts objectAtIndex: 1]);
-     NSLog(@"pct_remaining_left_side: %@", [kegdataparts objectAtIndex: 2]);
-     NSLog(@"pct_remaining_right_side: %@", [kegdataparts objectAtIndex: 3]);
-     */
-    
-    self.keg1temp.text = [NSMutableString stringWithFormat:@"%@ *F", [kegdataparts objectAtIndex: 0] ]; 
-    self.keg1pct.text = [NSMutableString stringWithFormat:@"%@ %%", [kegdataparts objectAtIndex: 2] ]; 
-    self.keg2temp.text = [NSMutableString stringWithFormat:@"%@ *F", [kegdataparts objectAtIndex: 1] ]; 
-    self.keg2pct.text = [NSMutableString stringWithFormat:@"%@ %%", [kegdataparts objectAtIndex: 3] ]; 
-    
+        self.keg1temp.text = [NSMutableString stringWithFormat:@"%@ \u00B0F", [kegdataparts objectAtIndex: 0] ]; 
+        self.keg1pct.text = [NSMutableString stringWithFormat:@"%@ %%", [kegdataparts objectAtIndex: 2] ]; 
+        self.keg2temp.text = [NSMutableString stringWithFormat:@"%@ \u00B0F", [kegdataparts objectAtIndex: 1] ]; 
+        self.keg2pct.text = [NSMutableString stringWithFormat:@"%@ %%", [kegdataparts objectAtIndex: 3] ]; 
+
         self.navBar.topItem.title = @"AppliedTrust Keg Status";
+        NSLog(@"Refresh finished!");
     }
-    NSLog(@"Refresh finished!");
 }
-
 
 @end
